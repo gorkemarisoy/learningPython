d70dd907ce8d31a73b95c6b3bd21d71251be32b3
A.Görkem ARISOY <79798735+gorkemarisoy@users.noreply.github.com>
2022-09-07 13:05:43 +0200

    Add files via upload

---
+++ ifElifElseExample.py
@@ -0,0 +1,7 @@
+﻿input = str(input("write me something :"))
+if input == "Spathiphyllum":
+    print("Yes - Spathiphyllum is the best plant ever!")
+elif input == "spathiphyllum":
+    print("No, I want a big Spathiphyllum!")
+else: 
+    print("Spathiphyllum! Not ", input,"!")
---
+++ whatKindOfYear.py
@@ -0,0 +1,19 @@
+﻿# if the year number isn't divisible by four, it's a common year;
+# otherwise, if the year number isn't divisible by 100, it's a leap year;
+# otherwise, if the year number isn't divisible by 400, it's a common year;
+# otherwise, it's a leap year.
+# if year is equal or smaller than 1580 it is "Not within the Gregorian calendar period"
+
+year = int(input("Enter a year: "))
+
+if year>1580:
+    if (year%4)!=0:
+        print("common year")
+    elif (year%100)!=0:
+        print("leap year")
+    elif (year%400)!=0:
+       print("common year")
+    else:
+       print("leap year")
+else:
+    print("Not within the Gregorian calendar period")
