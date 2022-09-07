46dd53d49a67497c6718e86504bbae895122a99b
A.Görkem ARISOY <79798735+gorkemarisoy@users.noreply.github.com>
2022-09-07 12:08:21 +0200

    Add files via upload

---
+++ BiggestNumber.py
@@ -0,0 +1,13 @@
+﻿nr1 = int(input("sayı 1 :"))
+nr2 = int(input("sayı 2 :"))
+nr3 = int(input("sayı 3 :"))
+
+if nr1>nr2:
+    biggestNr = nr1
+else:
+    biggestNr = nr2
+    
+if nr3>biggestNr:
+    biggestNr = nr3
+
+print("Biggest number is :" , biggestNr)
