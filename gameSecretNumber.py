26c4266bcedacc231cf2557d72c98fccc6a84ec1
A.Görkem ARISOY <79798735+gorkemarisoy@users.noreply.github.com>
2022-09-09 08:06:37 +0200

    Add files via upload

---
+++ gameSecretNumber.py
@@ -0,0 +1,20 @@
+﻿secret_number = 777
+
+print(
+"""
++================================+
+| Welcome to my game, muggle!    |
+| Enter an integer number        |
+| and guess what number I've     |
+| picked for you.                |
+| So, what is the secret number? |
++================================+
+""")
+
+guess = int(input("Guess the secret number...!\n"))
+
+while guess != secret_number:
+    print("Ha ha! You're stuck in my loop!")
+    guess = int(input("Guess the secret number...!\n"))
+    
+print("Well done, muggle! You are free now.")
