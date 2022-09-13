14ca8d704ca31b2912956eda3edfc5b3cdca6b41
A.Görkem ARISOY <79798735+gorkemarisoy@users.noreply.github.com>
2022-09-13 16:57:01 +0200

    Add files via upload

---
+++ removing_duplicates_and_sort_reverse_functions.py
@@ -0,0 +1,11 @@
+﻿my_list = [1, 2, 4, 4, 1, 4, 2, 6, 2, 9]
+
+for i in my_list:
+    if i in my_list:
+        del my_list[i]
+    
+
+print("The list with unique elements only:")
+my_list.sort()
+my_list.reverse()
+print(my_list)
