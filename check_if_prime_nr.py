a80d43e20b3626bbe1b9594e3d2d72f97a92887f
A.Görkem ARISOY <79798735+gorkemarisoy@users.noreply.github.com>
2022-09-16 16:17:10 +0200

    Add files via upload

---
+++ check_if_prime_nr.py
@@ -0,0 +1,14 @@
+﻿def is_prime(num):
+    s=True
+    for i in range(2,num):
+        if num%(i)==0:
+            return False
+    return s
+            
+# print(is_prime(15))
+
+for i in range(1, 20):
+	if is_prime(i + 1):
+			print(i + 1, end=" ")
+print()
+
