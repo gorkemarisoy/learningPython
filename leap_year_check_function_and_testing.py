47179472158b6f1aeb49181ea5b5641caf76a0ad
A.Görkem ARISOY <79798735+gorkemarisoy@users.noreply.github.com>
2022-09-16 14:04:34 +0200

    Add files via upload

---
+++ leap_year_check_function_and_testing.py
@@ -0,0 +1,25 @@
+﻿def is_year_leap(year):
+    if year//4 != 0:
+        if year//100 != 0:
+            return True
+    else:
+        return False
+#
+# put your code here
+#
+
+test_data = [1900, 2000, 2016, 1987]
+test_results = [False, True, True, False]
+for i in range(len(test_data)):
+	yr = test_data[i]
+	print(yr,"->",end="")
+	result = is_year_leap(yr)
+	if result == test_results[i]:
+		print("OK")
+	else:
+		print("Failed")
+
+# if the year number isn't divisible by four, it's a common year;
+# otherwise, if the year number isn't divisible by 100, it's a leap year;
+# otherwise, if the year number isn't divisible by 400, it's a common year;
+# otherwise, it's a leap year.
