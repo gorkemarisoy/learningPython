deffe8470dcc32cbaa6cd121bf9b962e60c9ccce
A.Görkem ARISOY <79798735+gorkemarisoy@users.noreply.github.com>
2022-09-20 15:03:17 +0200

    Add files via upload

---
+++ TICTACTOE.py
@@ -0,0 +1,36 @@
+﻿EMPTY = "-"
+Me = "O"
+Cp = "X"
+board = []
+
+for i in range(3):
+    row = [EMPTY for i in range(3)]
+    board.append(row)
+
+
+def display_board(board):
+    for i in range(3):
+        print(board[i])
+    
+display_board(board)
+    # The function accepts one parameter containing the board's current status
+    # and prints it out to the console.
+
+
+# def enter_move(board):
+#     # The function accepts the board's current status, asks the user about their move, 
+#     # checks the input, and updates the board according to the user's decision.
+
+
+# def make_list_of_free_fields(board):
+#     # The function browses the board and builds a list of all the free squares; 
+#     # the list consists of tuples, while each tuple is a pair of row and column numbers.
+
+
+# def victory_for(board, sign):
+#     # The function analyzes the board's status in order to check if 
+#     # the player using 'O's or 'X's has won the game
+
+
+# def draw_move(board):
+#     # The function draws the computer's move and updates the board.
