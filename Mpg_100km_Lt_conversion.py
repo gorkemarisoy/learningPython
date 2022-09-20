771b0dfc812273f7637dd916d29fdc818fa3f1c7
A.Görkem ARISOY <79798735+gorkemarisoy@users.noreply.github.com>
2022-09-20 14:45:48 +0200

    Add files via upload

---
+++ Mpg_100km_Lt_conversion.py
@@ -0,0 +1,20 @@
+﻿def liters_100km_to_miles_gallon(liters):
+    mpg = (100/1.609344)/(liters/3.785411784)
+    return mpg
+#
+# Write your code here.
+#
+
+def miles_gallon_to_liters_100km(miles):
+    kmsplt= (3.785411784*100) / (miles*1.609344)
+    return kmsplt
+#
+# Write your code here
+#
+
+print(liters_100km_to_miles_gallon(3.9))
+print(liters_100km_to_miles_gallon(7.5))
+print(liters_100km_to_miles_gallon(10.))
+print(miles_gallon_to_liters_100km(60.3))
+print(miles_gallon_to_liters_100km(31.4))
+print(miles_gallon_to_liters_100km(23.5))
